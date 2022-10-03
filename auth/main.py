6fe833c5fe18fb9df867d7b123fc7efc5bde9cb2
Николай Кириллович <Alset.Nikolas@yandex.ru>
2022-10-03 14:52:40 +0300

    add reg tel

--- auth/main.py
+++ auth/main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
-from fastapi import FastAPI
+from crypt import methods
+from flask import Flask, render_template, request, redirect
 from oauth import OAuthSignIn
 from auth_vk import VkSignIn
 from auth_github import GithubSignIn
 from auth_yandex import YandexSignIn
 import typing
+from auth_tel import get_token, SMSTransport, TSMSResponse
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "top secret!"
@@ -19,13 +21,32 @@ app.config["OAUTH_CREDENTIALS"] = {
         "secret": "3323a7456028481c9babb644928c6186",
     },
 }
-
+app.config["TOKEN_TEL"] = "E021CCF2-76F5-08B8-FD88-1D7FE93C87C6"
 TYPE_OAUTH = typing.Union[YandexSignIn, VkSignIn, GithubSignIn]
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/send_sms", methods=["GET", "POST"])
+def send_sms():
+    code: str = get_token()
+    tel: str = request.form.get("phone")
+    sms: SMSTransport = SMSTransport(app.config["TOKEN_TEL"])
+    result: TSMSResponse = sms.send(tel, code)
+    return redirect("/check_tel")
+
+
+@app.route("/check_tel", methods=["GET", "POST"])
+def registration_tel():
+    code: str = get_token()
+    if request.method == "POST":
+        if request.form.get("token") == code:
+            return "OK"
+        return render_template(f"auth_tel/check_token.html", er="Неправильный токен")
+    return render_template(f"auth_tel/check_token.html")
 
 
 @app.route("/authorize/<provider>")
